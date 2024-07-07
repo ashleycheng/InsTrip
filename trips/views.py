3e4fe3feefd12664f8284eb50f7fadbe20e060a1
ashleycheng <ashley830204@gmail.com>
2024-07-07 23:16:24 +0800

    feat: add a trip analysis page using Chart.js and chatGPT to generate content

--- trips/views.py
+++ trips/views.py
@@ -148,3 +148,19 @@ class TopLocationViewSet(mixins.ListModelMixin,
         if tag_fun_res:
             result.append({'tag': '休閒', 'location': tag_fun_res})
         return Response(result)
+
+
+def trip_analysis(request, country_name):
+    top_city_counter = get_top_city(country_name, 5)
+    labels = [i[0] for i in top_city_counter]
+    data = [i[1] for i in top_city_counter]
+
+    info = CountryInfo.objects.get(country_name=country_name)
+
+    return render(request, 'analysis.html', {
+        'country_name': country_name,
+        'country_name_ch': info.country_name_ch,
+        'analysis': info.analysis,
+        "labels": labels,
+        "data": data,
+    })
--- tripsite/urls.py
+++ tripsite/urls.py
@@ -5,7 +5,7 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework.permissions import IsAdminUser
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
-# from trips.views import city, country, home
+from trips.views import trip_analysis
     
 api_urlpatterns = [
     path('', include('trips.urls')),
@@ -26,8 +26,6 @@ urlpatterns = [
     )),
     path('api/', include((api_urlpatterns, 'api'))),
     path('auth/', include((auth_urlpatterns, 'api-auth'))),
-    # path("instrip/", home, name="home"),
-    # path("instrip/country/<str:country_name>/", country, name="country"),
-    # path("instrip/city/<str:city_name>/", city, name="city"),
+    path("analysis/<str:country_name>/", trip_analysis, name="trip_analysis"),
     path(r'', TemplateView.as_view(template_name="index.html")),
 ]
